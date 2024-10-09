b9756d50bcae8586dfd0d23ecf1bdd47b0f71ed4
FelipeCardozo0 <focardo@emory.edu>
2024-10-09 14:49:11 -0400

    Update ProblemSet4.java

--- ProblemSet4.java
+++ ProblemSet4.java
@@ -3,28 +3,36 @@ other students or code from online resources. Felipe Cardozo */
 
 public class ProblemSet4 {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("a mala nada na lama"));
 
-        System.out.println(encrypt("Secrets secrets secrets", 10));
-        System.out.println(decrypt("Wklv lv pv vhfuhw phvvdjh", 3));
-        System.out.println(decrypt("IY 7=6 oy znk hkyz irgyy", 6));
-        System.out.println(decrypt("}om|o~} }om|o~} }om|o~}", 10));
+        System.out.println(encrypt("Botafogo", 3));
+        System.out.println(decrypt("Erwdirjr", 3));
+
         System.out.println(encrypt("This is my secret message", 3));
-        System.out.println(encrypt("CS 170 is the best class", 6));
+        System.out.println(decrypt("Wklv lv pb vhfuhw phvvdjh" , 3));
 
+        System.out.println(decrypt("Wklv lv p| vhfuhw phvvdjh, 3)" , 3));
+        System.out.println(decrypt("decrypt(IY 7=6 oy znk hkyz irgyy", 6));
+        System.out.println(decrypt("}om|o~} }om|o~} }om|o~}", 10));
+        System.out.println(decrypt("CS 170 is the best class", 6));
+
+        System.out.println(decrypt("Neze tvskveqqmrk mw ws qygl jyr", 4));
+
+        System.out.println(guess_shift("T ^_`Od NZX[`_P] ^NTPYNPL_ PXZ]d"));
+        System.out.println(decrypt("T ^_`Od NZX[`_P] ^NTPYNPL_ PXZ]d", 4));
 
     }
 
     public static boolean isPalindrome(String text) {
         String reverse = "";
         text = text.toLowerCase();
-        text = text.replace(" ", ""); //cant change a string, make new string
-
+        text = text.replace(" ","");
         for (int i = text.length() - 1; i >= 0; i--) {
             reverse += text.charAt(i);
         }
 
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != reverse.charAt(i)) {
+            if (text.charAt(i) != reverse.charAt(i)){
                 return false;
             }
         }
@@ -32,60 +40,90 @@ public class ProblemSet4 {
     }
 
     public static String encrypt(String text, int shift) {
-        StringBuilder encryptedtext = new StringBuilder();
+        StringBuilder encryptedText = new StringBuilder();
 
-        text = text.toLowerCase();
-        for (int i = 0; i < text.length(); i++)
-        {
-            char c = text.charAt(i);
-            if (Character.isLetter (c))
-            {
-                c = (char) ((c - 'a' + shift +26) % 26 + 'a');
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (!Character.isWhitespace(ch)) {
+                if (Character.isLetter(ch)) {
+                    if (Character.isUpperCase(ch)) {
+                        char shiftedChar = (char) ((ch - 'A' + shift) % 26 + 'A');
+                        encryptedText.append(shiftedChar);
+
+
+
+                    } else {
+
+                        char shiftedChar = (char) ((ch - 'a' + shift) % 26 + 'a');
+                        encryptedText.append(shiftedChar);
+                    }
+                } else {
+                    encryptedText.append(ch);
+                }
+            } else {
+                encryptedText.append(ch);
             }
-            encryptedtext.append(c);
         }
 
-        return encryptedtext.toString();
+        return encryptedText.toString();
     }
 
     public static String decrypt(String text, int shift) {
-        StringBuilder decryptedtext = new StringBuilder();
+        StringBuilder decryptedText = new StringBuilder();
 
-        text = text.toLowerCase();
         for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (Character.isLetter(c)) {
-                c = (char) ((c - 'a' - shift + 26) % 26 + 'a');
-            }
-            decryptedtext.append(c);
+            char ch = text.charAt(i);
+
+            if (!Character.isWhitespace(ch)) {
+                if (Character.isLetter(ch)) {
+                    if (Character.isUpperCase(ch)) {
+
+                        char shiftedChar = (char) ((ch - 'A' - shift + 26) % 26 + 'A');
+                        decryptedText.append(shiftedChar);
+                    } else {
+                        char shiftedChar = (char) ((ch - 'a' - shift + 26) % 26 + 'a');
+                        decryptedText.append(shiftedChar);
+                    }
+                } else {
+                    decryptedText.append(ch);
+                }
+            } else {
+                decryptedText.append(ch);             }
         }
 
-        return decryptedtext.toString();
+        return decryptedText.toString();
     }
-
 
     public static String guess_shift(String text) {
         StringBuilder result = new StringBuilder();
 
-        // Try all shift values from 1 to 26
         for (int shift = 1; shift <= 26; shift++) {
             StringBuilder decryptedText = new StringBuilder();
 
-            // Loop through each character in the text
             for (int i = 0; i < text.length(); i++) {
                 char ch = text.charAt(i);
 
-                // Shift every character back by the current shift value
-                char shiftedChar = (char) (ch - shift);
-
-                // Append the shifted character to the result
-                decryptedText.append(shiftedChar);
+                if (!Character.isWhitespace(ch)) {
+                    if (Character.isLetter(ch)) {
+                        if (Character.isUpperCase(ch)) {
+                            char shiftedChar = (char) ((ch - 'A' - shift + 26) % 26 + 'A');
+                            decryptedText.append(shiftedChar);
+                        } else {
+                            char shiftedChar = (char) ((ch - 'a' - shift + 26) % 26 + 'a');
+                            decryptedText.append(shiftedChar);
+                        }
+                    } else {
+                        decryptedText.append(ch);
+                    }
+                } else {
+                    decryptedText.append(ch);
+                }
             }
-
-            // Append the shift value and decrypted text to the result
             result.append("Shift ").append(shift).append(": ").append(decryptedText.toString()).append("\n");
         }
 
         return result.toString();
     }
+
 }
