d2d5bd0a616c41541610479b2006375a6cd0c5f5
FelipeCardozo0 <focardo@emory.edu>
2024-10-09 15:33:14 -0400

    Update ProblemSet4.java

--- ProblemSet4.java
+++ ProblemSet4.java
@@ -22,31 +22,31 @@ public class ProblemSet4 {
 
     }
 
-    public static boolean isPalindrome(String text) {
-        String reverse = "";
-        text = text.toLowerCase();
-        text = text.replace(" ","");
-        for (int i = text.length() - 1; i >= 0; i--) {
+    public static boolean isPalindrome(String text){
+        String reverse ="";
+        text=text.toLowerCase();
+        text =text.replace(" ","");
+        for (int i =text.length() - 1; i>= 0; i--){
             reverse += text.charAt(i);
         }
 
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != reverse.charAt(i)){
+        for (int i = 0; i <text.length(); i++){
+            if (text.charAt(i) !=reverse.charAt(i)){
                 return false;
             }
         }
         return true;
     }
-    public static String encrypt(String text, int shift) {
+    public static String encrypt(String text, int shift){
         StringBuilder encryptedText = new StringBuilder();
 
-        for (int i = 0; i < text.length(); i++) {
-            char currentChar = text.charAt(i);
+        for (int i =0; i<text.length(); i++){
+            char currentChar =text.charAt(i);
 
-            if (Character.isWhitespace(currentChar)) {
+            if (Character.isWhitespace(currentChar)){
                 encryptedText.append(currentChar);
             } else {
-                char shiftedChar = (char) (currentChar + shift);
+                char shiftedChar=(char) (currentChar +shift);
                 encryptedText.append(shiftedChar);
             }
         }
@@ -54,13 +54,13 @@ public class ProblemSet4 {
         return encryptedText.toString();
     }
 
-    public static String decrypt(String text, int shift) {
-        StringBuilder decryptedText = new StringBuilder();
+    public static String decrypt(String text,int shift){
+        StringBuilder decryptedText=new StringBuilder();
 
-        for (int i = 0; i < text.length(); i++) {
+        for (int i =0; i<text.length();i++){
             char currentChar = text.charAt(i);
 
-            if (Character.isWhitespace(currentChar)) {
+            if (Character.isWhitespace(currentChar)){
                 decryptedText.append(currentChar);
             } else {
                 char shiftedChar = (char) (currentChar - shift);
@@ -71,22 +71,23 @@ public class ProblemSet4 {
         return decryptedText.toString();
     }
 
-    public static void guess_shift(String text) {
-        for (int shift = 1; shift <= 26; shift++) {
+    public static void guess_shift(String text){
+        for (int shift = 1; shift <= 26; shift++){
             StringBuilder decryptedText = new StringBuilder();
 
-            for (int i = 0; i < text.length(); i++) {
+            for(int i=0; i<text.length(); i++){
                 char currentChar = text.charAt(i);
 
-                if (Character.isWhitespace(currentChar)) {
+                if(Character.isWhitespace(currentChar)){
                     decryptedText.append(currentChar);
-                } else {
-                    char shiftedChar = (char) (currentChar - shift);
+                }
+                else{
+                    char shiftedChar=(char)(currentChar -shift);
                     decryptedText.append(shiftedChar);
                 }
             }
 
-            System.out.println("Shift " + shift + ": " + decryptedText.toString());
+            System.out.println("shift "+shift+ ": " + decryptedText.toString());
         }
     }
 }
